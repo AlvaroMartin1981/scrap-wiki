d71e0fa39b9d50f277df0d62462ffed24b095fa6
titopope@hotmail.com <titopope@hotmail.com>
2024-01-25 19:45:03 +0100

    hice lo que pude

--- index.js
+++ index.js
@@ -1,26 +1,59 @@
 const axios = require('axios');
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
 
-const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap'
 
-app.get('/',(req,res)=>{
-    axios.get(url).then((response)=>{
-        if(response.status ===200){
-            const html = response.data
-            const $= cheerio.load(html);
-            const pageTitle= $('title').text()
-            res.send(`
-            <h1>${pageTitle}</h1>`)
-        }
-       
-;
-    })
-})
+const url = 'https://es.wikipedia.org/wiki/Categor%C3%ADa:M%C3%BAsicos_de_rap';
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    axios.get(url).then((response) => {
+        if (response.status === 200) {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const enlaces = [];
+            
+            
+            $('#mw-pages a').each((index, element) => {
+                const enlace = "https://es.wikipedia.org" + $(element).attr('href');
+                enlaces.push(enlace);
+            });
+            const datosMusicos = enlaces.map(enlace =>{
+                return axios.get(enlace).then((response)=>{
+                    if (response.status === 200){
+                        const html2 =response.data
+                        const $2= cheerio.load(html2);
+                        const pagesTitles = $2('title').text()
+                        const imgs = [];
+                        const textos= $2('p').text()
+                        $2('img').each((index, element)=>{
+                            const img = $(element).attr('src');
+                            imgs.push(img);
+                        });
+                        return {url: enlaces, pagesTitles, imgs, textos};
+                    } else {
+                        res.status(404).send('Error al obtener la página principal');
+                    }
+                })
+            });
+        }});
+    });        
 
-app.listen(3000, ()=>{
-    console.log("Express esta escuchando en el puerto http://localhost3000");
+app.listen(3000, () => {
+    console.log(`Express está escuchando en el puerto http://localhost:3000`);
 });
+                      
+
+
+            //res.json(enlaces)
+            /*esto esta mal porque me salen los enlaces pero no me abre bien los enlaces
+            res.send(`<h2>Enlaces</h2>
+            <ul>${enlaces.map((enlace)=>`<li><a href="${url}${enlace}">${enlace}</a></li>`).join('')}
+            </ul>            
+            `);*/
+            
+          
+    
